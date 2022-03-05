719d4178540111f1a42d94acd259babc1e658f94
KeiraAkuan <77209368+KeiraAkuan@users.noreply.github.com>
2022-03-05 14:58:05 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -30,14 +30,15 @@ assets.image`Cotton Candy Donut`
 scene.setBackgroundColor(9)
 tiles.setTilemap(tilemap`level`)
 let baseDonut = sprites.create(DonutImgs[randint(0, DonutImgs.length - 1)], SpriteKind.StackDonut)
+let TopDonut = baseDonut
 baseDonut.ay = 300
 baseDonut.setPosition(80, 600)
 scene.cameraFollowSprite(baseDonut)
 
 let newDonut: Sprite = null
 function createNewDonut () {
-    newDonut = sprites.create(DonutImgs[randint(0, DonutImgs.length - 1)], SpriteKind.StackDonut)
-    newDonut.setPosition(randint(20, 140), baseDonut.y - 20)
+    newDonut = sprites.create(DonutImgs[randint(0, DonutImgs.length - 1)], SpriteKind.Donut)
+    newDonut.setPosition(randint(20, 140), TopDonut.y - 20)
     
     if (Math.percentChance(50)) {
         newDonut.vx = randint(-50, -100)
@@ -52,4 +53,15 @@ function createNewDonut () {
 controller.A.onEvent(ControllerButtonEvent.Pressed, function() {
     newDonut.vx = 0
     newDonut.ay = 300
+})
+sprites.onOverlap(SpriteKind.StackDonut, SpriteKind.Donut, function(sprite: Sprite, DroppedDonut: Sprite) {
+    DroppedDonut.ay = 0
+    DroppedDonut.vy = 0
+    DroppedDonut.setKind(SpriteKind.StackDonut)
+    TopDonut = DroppedDonut
+    createNewDonut()
+    scene.cameraFollowSprite(TopDonut)
+})
+scene.onHitWall(SpriteKind.Donut, function(sprite: Sprite, location: tiles.Location) {
+    
 })
